2a02a10863a981294fe35e287fa878e038038741
Helmiina <helmiina.hotti@hotmail.com>
2021-09-13 13:15:32 +0300

    Prettified forecast.py

--- forecast.py
+++ forecast.py
@@ -3,26 +3,32 @@ import pandas as pd
 from fmiopendata.wfs import download_stored_query
 import datetime as dt
 
-start_time = dt.datetime.now()
-end_time = start_time + dt.timedelta(hours=58)
-print(start_time, end_time)
+def get_forecast():
+    start_time = dt.datetime.now()
+    end_time = start_time + dt.timedelta(hours=58)
+    print(f"Getting weather forecast for {start_time} - {end_time}...\n")
 
-# The weather forecast is streamed from the FMI open data and seems to give info about the upcoming ~58 hours
-futuredata = download_stored_query("fmi::forecast::harmonie::surface::point::multipointcoverage",
-                                   args=["place=helsinki", "starttime=" + str(start_time),
-                                         "endtime=" + str(end_time)])
+    # The weather forecast is streamed from the FMI open data and seems to give info about the upcoming ~58 hours
+    futuredata = download_stored_query("fmi::forecast::harmonie::surface::point::multipointcoverage",
+                                    args=["place=helsinki", "starttime=" + str(start_time),
+                                            "endtime=" + str(end_time)])
 
-indices = []
-rows = []
-for item in futuredata.data.items():
-    indices.append(item[0])
-    row = item[1]['Helsinki']
-    for key in list(row.keys()):
-        cell = f"{list(row[key].values())[0]}"
-        row[f"{key} ({list(row[key].values())[1]})"] = cell
-        del row[key]
-    rows.append(row)
+    indices = []
+    rows = []
+    for item in futuredata.data.items():
+        indices.append(item[0])
+        row = item[1]['Helsinki']
+        for key in list(row.keys()):
+            cell = f"{list(row[key].values())[0]}"
+            row[f"{key} ({list(row[key].values())[1]})"] = cell
+            del row[key]
+        rows.append(row)
 
-df = pd.DataFrame(data=rows, index=indices)
+    # Store hourly info to a time series dataframe
+    df = pd.DataFrame(data=rows, index=indices)
+    return df
 
-print(df)
+
+if __name__ == "__main__":
+    forecast = get_forecast()
+    print(forecast)
